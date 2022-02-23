aca064b545523039800e23a36121f2dfeeae64bd
sayedalfeki <selfeki207@gmail.com>
2022-02-23 23:56:56 +0200

    zeet

--- start.js
+++ start.js
@@ -2,9 +2,9 @@ const user_router=require('./routers/user.router')
 const sample_router=require('./routers/sample.router')
 const express=require('express')
 const bodyparser=require('body-parser')
-
+const host='0.0.0.0'
 const app=express()
-let port=process.env.port||3030
+let port=3000
 
 app.use(bodyparser.urlencoded({extended:true}))
 app.use(bodyparser.json({extended:true}))
@@ -17,7 +17,7 @@ res.json({
 app.use('/user',user_router)
 app.use('/sample',sample_router)
 
-app.listen(port,()=>{
+app.listen(port,host,()=>{
     console.log(new Date().getDate())
     console.log('server started with port '+port)
 })
